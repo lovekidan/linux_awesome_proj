05f65cbac3ebf273f6241c374a1d9e8971ff6a63
lovekidan <lovekidan@qq.com>
2016-02-22 20:53:08 +0800

    add calc_lib

---
+++ include/cacl/cacl_util.hpp
@@ -0,0 +1,6 @@
+#ifndef CALC_UTIL
+#define CALC_UTIL
+
+int calc_sum(int arg1, int arg2);
+
+#endif
---
+++ src/lib/calc/calc_util.cpp
@@ -0,0 +1,5 @@
+#include "../../../../include/calc_util.h"
+
+int calc_sum(int arg1, int arg2) {
+	return arg1 + arg2;
+}
